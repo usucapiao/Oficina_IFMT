8ac84504a01bacd2f5b6ce82a4b5f763bcf4017c
mufasaDev0 <mateus.oliveira@fisica.ufmt.br>
2025-09-26 17:49:02 -0400

    Lista de nomes na tela

---
+++ lista_nomes.py
@@ -0,0 +1,8 @@
+
+# Lista de nomes de quem modificou o arquivo
+nomes = ["Mateus"]
+
+# Exibir os nomes na tela
+print("Lista de nomes de quem modificou o arquivo:")
+for nome in nomes:
+    print("-", nome)
